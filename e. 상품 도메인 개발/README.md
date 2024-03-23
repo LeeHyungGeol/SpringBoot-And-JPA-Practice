df4aecf850e37be08fce11a5676f510fe9639229
LeeHyungGeol <gudrjf1138@gmail.com>
2024-03-24 03:50:29 +0900

    feat(상품-도메인-개발): 상품 서비스 개발

--- e. 상품 도메인 개발/README.md
+++ e. 상품 도메인 개발/README.md
@@ -88,3 +88,27 @@ public class ItemRepository {
     }
 }
 ```
+
+## 상품 서비스 개발
+
+```java
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void save(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.find(itemId);
+    }
+}
+```
---
+++ src/main/java/com/example/jpabook/service/ItemService.java
@@ -0,0 +1,28 @@
+package com.example.jpabook.service;
+
+import com.example.jpabook.domain.item.Item;
+import com.example.jpabook.repository.ItemRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void save(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.find(itemId);
+    }
+}
